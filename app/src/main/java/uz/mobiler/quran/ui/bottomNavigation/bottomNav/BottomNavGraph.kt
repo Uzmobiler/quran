d0246249670f847dfc359bf72960e2383040a56b
Eshmurodov-Doston1 <76496460+Eshmurodov-Doston1@users.noreply.github.com>
2022-01-30 17:11:20 +0500

    New Comet

--- app/src/main/java/uz/mobiler/quran/ui/bottomNavigation/bottomNav/BottomNavGraph.kt
+++ app/src/main/java/uz/mobiler/quran/ui/bottomNavigation/bottomNav/BottomNavGraph.kt
@@ -1,19 +1,24 @@
 package uz.mobiler.quran.ui.bottomNavigation.bottomNav
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import uz.mobiler.quran.ui.bottomNavigation.bottomBar.BottomBarScreen
 import uz.mobiler.quran.ui.pages.*
+import uz.mobiler.quran.vm.SurahViewModel
 
 @Composable
 fun BottomNavGraph(navController: NavHostController){
     NavHost(navController = navController, startDestination = BottomBarScreen.Home.route){
-        composable(route = BottomBarScreen.Home.route){
-            HomeScreen()
-        }
+            composable(route = BottomBarScreen.Home.route){
+                val exampleViewModel = hiltViewModel<SurahViewModel>()
+                HomeScreen(exampleViewModel)
+            }
         composable(route = BottomBarScreen.Quran.route){
             QuranScreen()
         }
--- app/src/main/java/uz/mobiler/quran/ui/pages/HomeScreen.kt
+++ app/src/main/java/uz/mobiler/quran/ui/pages/HomeScreen.kt
@@ -1,36 +1,79 @@
 package uz.mobiler.quran.ui.pages
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.*
+import androidx.hilt.navigation.compose.*
+import kotlinx.coroutines.flow.collect
 import uz.mobiler.quran.models.surah.Data
 import uz.mobiler.quran.ui.rows.SurahRow
 import uz.mobiler.quran.vm.SurahResource
+import uz.mobiler.quran.vm.SurahResource.*
 import uz.mobiler.quran.vm.SurahViewModel
 
+
+val listCard: MutableState<List<Data>> = mutableStateOf(emptyList())
+
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun HomeScreen() {
-    val myViewModel = hiltViewModel<SurahViewModel>()
-    val value = myViewModel.getSurah().collectAsState().value
-    value.let {
-        when (it) {
-            is SurahResource.Success -> {
-                val surahData = it.surahData
-                LazyColumn {
-                    items(surahData?.data ?: emptyList()) { data ->
-                        SurahRow(data = data)
-                    }
+fun HomeScreen(viewModel: SurahViewModel = hiltViewModel()) {
+//    val exampleViewModel = hiltViewModel<SurahViewModel>()
+
+    LaunchedEffect(Unit){
+        viewModel.getSurah().collect {
+            when(it){
+                is SurahResource.Success->{
+                    Log.e("DAtalar", it.surahData?.data.toString() )
+                    listCard.value = it.surahData?.data!!
                 }
-            }
-            is SurahResource.Error -> {
+                is  SurahResource.Loading->{
 
-            }
-            SurahResource.Loading -> {
+                }
+                is SurahResource.Error->{
 
+                }
             }
         }
     }
+    LoadData()
+
+
+    //val myViewModel = hiltViewModel<SurahViewModel>()
+   // val value = myViewModel.getSurah().collectAsState().value
+//    value.let {
+//
+//    }
+//       when(it){
+//           is SurahResource.Success->{
+//               Log.e("Datalar", it.surahData?.data?.toString().toString())
+//           }
+//           is SurahResource.Loading->{
+//
+//           }
+//
+//           is SurahResource.Error->{
+//
+//           }
+//       }
+
+
 }
+
+@Composable
+fun LoadData(){
+    LazyColumn{
+        items(listCard.component1()) { data ->
+            SurahRow(data = data)
+        }
+    }
+}
+
+@Composable
+fun LoadData(data: List<Data>?) {
+
+}
+
+
+
